0b3f2236b183cbba4c669b35a3fceca16c9a1b78
Andi Rückauer <arueckauer@gmail.com>
2024-01-12 13:11:47 +0100

    Adds code causing issues

---
+++ src/UpsertFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use const PHP_EOL;
+
+class Foo
+{
+    private static string $bar = '' . PHP_EOL
+    . 'a' . PHP_EOL
+    . 'b' . PHP_EOL
+    . 'c' . PHP_EOL
+    . 'd' . PHP_EOL
+    . 'e' . PHP_EOL
+    . 'f' . PHP_EOL
+    . 'g' . PHP_EOL
+    . 'h' . PHP_EOL
+    . 'i' . PHP_EOL
+    . 'j' . PHP_EOL
+    . 'k' . PHP_EOL
+    . 'l' . PHP_EOL
+    . 'm' . PHP_EOL
+    . 'n' . PHP_EOL
+    . 'o' . PHP_EOL
+    . 'p' . PHP_EOL
+    . 'q' . PHP_EOL
+    . 'r' . PHP_EOL
+    . 's' . PHP_EOL
+    . 't' . PHP_EOL
+    . 'u' . PHP_EOL
+    . 'v' . PHP_EOL
+    . 'w' . PHP_EOL
+    . 'x' . PHP_EOL
+    . 'y' . PHP_EOL
+    . 'z' . PHP_EOL
+    . '0';
+
+}
